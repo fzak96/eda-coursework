ea9883d0c49996a85c9d4b5a418d652b98ae51d8
Farhan Zaki <farhan.zaki.24@ucl.ac.uk>
2024-11-21 15:53:43 +0000

    fix terraform output file

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output mgmt_vm_ids {
-  value = harvester_virtualmachine.mgmtvm[*].network_interface[*].ip_address
+output mgmt_vm_ips {
+  value = harvester_virtualmachine.mgmtvm[*].network_interface[0].ip_address
 }
 
-output mgmt_vm_ips {
+output mgmt_vm_ids {
   value = harvester_virtualmachine.mgmtvm.*.id
 }
 
-output worker_vm_ids {
-  value = harvester_virtualmachine.workervm[*].network_interface[*].ip_address
+output worker_vm_ips {
+  value = harvester_virtualmachine.workervm[*].network_interface[0].ip_address
 }
 
-output worker_vm_ips {
+output worker_vm_ids {
   value = harvester_virtualmachine.workervm.*.id
 }
