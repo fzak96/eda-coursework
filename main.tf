d23b5effed50319b57435a5ae3c0bc98f98c61e9
Farhan Zaki <129845187+fzak96@users.noreply.github.com>
2024-11-21 13:49:32 +0000

    add files via upload to git repo

---
+++ main.tf
@@ -0,0 +1,77 @@
+data "harvester_image" "img" {
+  display_name = var.img_display_name
+  namespace    = "harvester-public"
+}
+
+data "harvester_ssh_key" "mysshkey" {
+  name      = var.keyname
+  namespace = var.namespace
+}
+
+resource "random_id" "secret" {
+    byte_length = 5
+}
+
+resource "harvester_cloudinit_secret" "cloud-config" {
+  name      = "cloud-config-${random_id.secret.hex}"
+  namespace = var.namespace
+
+  user_data = templatefile("cloud-init.tmpl.yml", {
+      public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
+    })
+}
+
+resource "harvester_virtualmachine" "hostvm" {
+  
+  count = 1
+
+  name                 = "${var.username}-host-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = var.namespace
+  restart_after_update = true
+
+  description = "Cluster Host Node"
+
+  cpu    = 2 
+  memory = "4Gi"
+
+  efi         = true
+  secure_boot = false
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-host-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = var.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "50Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  disk {
+    name       = "datadisk"
+    type       = "disk"
+    size       = "80Gi"
+    bus        = "virtio"
+    boot_order = 2
+
+    auto_delete = true
+  }
+
+    cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable img_display_name {
+  type = string
+  default = "almalinux-9.4-20240805"
+}
+
+variable namespace {
+  type = string
+  default = "ucabfz0-comp0235-ns"
+}
+
+variable network_name {
+  type = string
+  default = "ucabfz0-comp0235-ns/ds4eng"
+}
+
+variable username {
+  type = string
+ default = "ucabfz0"
+}
+
+variable keyname {
+  type = string
+  default = "farhanzaki-cnc"
+}
