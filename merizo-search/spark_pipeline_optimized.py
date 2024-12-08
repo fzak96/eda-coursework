e4d055e2b6cec4854fb73533e406b6d34fd21470
Farhan Zaki <farhan.zaki.24@ucl.ac.uk>
2024-12-08 04:27:21 +0000

    fix sys argument reference

--- merizo-search/spark_pipeline_optimized.py
+++ merizo-search/spark_pipeline_optimized.py
@@ -102,7 +102,9 @@ def run_parser(file_name, current_dir):
     with open(search_file_path, "r") as fhIn:
         next(fhIn)
         msreader = csv.reader(fhIn, delimiter='\t',)
+        tot_entries = 0
         for i, row in enumerate(msreader):
+            tot_entries = i+1
             plDDT_values.append(float(row[3]))
             meta = row[15]
             data = json.loads(meta)
@@ -115,9 +117,9 @@ def run_parser(file_name, current_dir):
             output_file = os.path.join(temp_dir, f"{file_name}.parsed")
             with open(output_file, "w", encoding="utf-8") as fhOut:
                 if len(plDDT_values) > 0:
-                    fhOut.write(f"#{sys.argv[2]} Results. mean plddt: {statistics.mean(plDDT_values)}\n")
+                    fhOut.write(f"#{search_file} Results. mean plddt: {statistics.mean(plDDT_values)}\n")
                 else:
-                    fhOut.write(f"#{sys.argv[2]} Results. mean plddt: 0\n")
+                    fhOut.write(f"#{search_file} Results. mean plddt: 0\n")
                 fhOut.write("cath_id,count\n")
                 for cath, v in cath_ids.items():
                     fhOut.write(f"{cath},{v}\n")
